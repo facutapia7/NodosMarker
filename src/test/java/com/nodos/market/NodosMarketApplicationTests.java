6bf665f82b743beddce8cb175239e5ba6fdf4dfa
facutapia7 <facutapia7@gmail.com>
2022-06-24 09:20:38 -0300

    Completada la extructura de la persistencia

---
+++ src/test/java/com/nodos/market/NodosMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nodos.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodosMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
